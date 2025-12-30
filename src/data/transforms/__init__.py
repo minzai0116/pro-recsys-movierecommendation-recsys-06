bd3bd7ad2b8c55ec57499f0268449b29d13aa77f
timesmoker <timesmoker@gmail.com>
2025-12-30 06:42:54 +0000

    chore: update .gitignore and add package documentation for transforms

--- src/data/transforms/__init__.py
+++ src/data/transforms/__init__.py
@@ -1,2 +1,9 @@
-from . import global_transform  # noqa: F401
-from . import post_transform    # noqa: F401
+"""
+`src.data.transforms` package.
+
+주의:
+- 여기서 하위 모듈을 강제 import 하지 않습니다.
+- transform 등록은 `src.data.transforms.registry.bootstrap_transforms()`(autodiscover)가 담당합니다.
+"""
+
+__all__: list[str] = []
